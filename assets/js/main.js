81fb4b4bfef131fa89f77398a59b3c9723d7b1b6
4z4quality <4z4quality@gmail.com>
2022-05-27 03:53:23 +0300

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,3 +277,17 @@
   });
 
 })()
+
+  /**
+   * Hide Header on scroll
+   */
+      var prevScrollpos = window.pageYOffset;
+      window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+          document.getElementById("header").style.top = "0";
+        } else {
+          document.getElementById("header").style.top = "-91px";
+        }
+        prevScrollpos = currentScrollPos;
+      }
