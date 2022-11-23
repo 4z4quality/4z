0c6471f83d94072ec09bef8ffd9c967961d0d06d
salsabil2022 <marinapharma@hotmail.com>
2022-11-23 12:06:16 +0300

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,8 +159,8 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('bx-menu')
+    this.classList.toggle('bx-x')
   })
 
   /**
@@ -184,8 +184,8 @@
       if (navbar.classList.contains('navbar-mobile')) {
         navbar.classList.remove('navbar-mobile')
         let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        navbarToggle.classList.toggle('bx-menu')
+        navbarToggle.classList.toggle('bx-x')
       }
       scrollto(this.hash)
     }
@@ -205,12 +205,12 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+   const removePreloader = () => preloader.remove();
+   let preloader = select("#preloader");
+   if (preloader) {
+     window.addEventListener("load", () => setTimeout(removePreloader, 1000));
+   }
+
 
   /**
    * Initiate glightbox
