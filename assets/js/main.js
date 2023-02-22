ed1df26c927fde289ca8b04e53c73bb2858674f5
4z4quality <4z4quality@gmail.com>
2023-02-22 11:56:36 +0300

    y

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,32 +2,27 @@
 */
 (function() {
   "use strict";
-
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el?.trim()
     if (all) {
       return [...document.querySelectorAll(el)]
     } else {
       return document.querySelector(el)
     }
   }
-
   /**
    * Intro carousel indicators
    */
   let introCarouselIndicators = select("#intro-carousel-indicators")
   let introCarouselItems = select('#introCarousel .carousel-item', true)
-
   introCarouselItems.forEach((item, index) => {
     (index === 0) ?
     introCarouselIndicators.innerHTML += "<li data-bs-target='#introCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
       introCarouselIndicators.innerHTML += "<li data-bs-target='#introCarousel' data-bs-slide-to='" + index + "'></li>"
   });
-
-
  /**
 * Hide Header on scroll
 */
@@ -41,8 +36,6 @@
     }
     prevScrollpos = currentScrollPos;
   }
-
-
   /**
    * Easy event listener function
    */
@@ -56,14 +49,12 @@
       }
     }
   }
-
   /**
    * Easy on scroll event listener 
    */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
-
   /**
    * Navbar links active state on scroll
    */
@@ -83,25 +74,21 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
-
   /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
-
     if (!header.classList.contains('header-scrolled')) {
       offset -= 16
     }
-
     let elementPos = select(el).offsetTop
     window.scrollTo({
       top: elementPos - offset,
       behavior: 'smooth'
     })
   }
-
   /**
    * Header fixed top on scroll
    */
@@ -121,7 +108,6 @@
     window.addEventListener('load', headerFixed)
     onscroll(document, headerFixed)
   }
-
   /**
    * Back to top button
    */
@@ -137,7 +123,6 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
-
   /**
    * Callout
    */
@@ -153,7 +138,6 @@
     window.addEventListener('load', togglecallout)
     onscroll(document, togglecallout)
   }
-
   /**
    * Mobile nav toggle
    */
@@ -162,7 +146,6 @@
     this.classList.toggle('bx-menu')
     this.classList.toggle('bx-x')
   })
-
   /**
    * Mobile nav dropdowns activate
    */
@@ -172,14 +155,12 @@
       this.nextElementSibling.classList.toggle('dropdown-active')
     }
   }, true)
-
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
-
       let navbar = select('#navbar')
       if (navbar.classList.contains('navbar-mobile')) {
         navbar.classList.remove('navbar-mobile')
@@ -190,7 +171,6 @@
       scrollto(this.hash)
     }
   }, true)
-
   /**
    * Scroll with ofset on page load with hash links in the url
    */
@@ -201,7 +181,6 @@
       }
     }
   });
-
   /**
    * Preloader
    */
@@ -211,7 +190,6 @@
       preloader.remove()
     });
   }
-
   /**
    * Initiate glightbox
    */
@@ -235,7 +213,6 @@
       }
     })
   }
-
   /**
    * Testimonials slider
    */
@@ -253,7 +230,6 @@
       clickable: true
     }
   });
-
   /**
    * Porfolio isotope and filter
    */
@@ -263,9 +239,7 @@
       let portfolioIsotope = new Isotope(portfolioContainer, {
         itemSelector: '.portfolio-item'
       });
-
       let portfolioFilters = select('#portfolio-flters li', true);
-
       on('click', '#portfolio-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
@@ -281,16 +255,13 @@
         });
       }, true);
     }
-
   });
-
   /**
    * Initiate portfolio lightbox 
    */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
-
   /**
    * Portfolio details slider
    */
@@ -307,7 +278,6 @@
       clickable: true
     }
   });
-
   /**
    * Animation on scroll
    */
@@ -319,5 +289,4 @@
       mirror: false
     })
   });
-
 })()
